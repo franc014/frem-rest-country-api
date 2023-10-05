9bd9c706b040125bb8650efa3cbaff69cf56c42a
JUan Andrade <jfandradea@gmail.com>
2023-10-05 13:11:36 -0500

    generating detail pages

--- src/pages/api/countries.json.js
+++ src/pages/api/countries.json.js
@@ -1,14 +1,12 @@
 export async function GET({ params, request }) {
     
-    const fields = `name,population,region,capital,flags`;
+    const fields = `name,population,region,capital,flags,cca2`;
     const countriesAPIResponse = await fetch(`https://restcountries.com/v3.1/all?fields=${fields}`);
     const countries = await countriesAPIResponse.json();
 
     const countriesJSON = JSON.stringify(countries);
 
     const countriesArray = JSON.parse(countriesJSON);
-
-    console.log(countriesArray);
 
     const sortedCountries = countriesArray.sort((a, b) => {
         return b.name.common < a.name.common ? 1 : -1;
