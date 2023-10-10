4f7860ca1fc215a31a1630f2302711434e29aaf8
JUan Andrade <jfandradea@gmail.com>
2023-10-09 19:29:49 -0500

    finish country search

--- src/components/Svelte/Autocomplete.svelte
+++ src/components/Svelte/Autocomplete.svelte
@@ -4,17 +4,28 @@
 
 	async function searchCountry(e) {
 		searchTerm = e.target.value;
-		const countriesApiResponse = await fetch(`http://localhost:4321/api/search/${searchTerm}.json`);
+		const countriesAPI = `http://localhost:4321/api/search/${searchTerm}.json`;
+		try {
+			const countriesApiResponse = await fetch(countriesAPI);
 
-		const countriesJson = await countriesApiResponse.json();
+			if (countriesApiResponse.status === 404) {
+				console.error('No API found for the search term');
+				foundCountries = [];
+			}
 
-		console.log(countriesJson);
+			const countriesJson = await countriesApiResponse.json();
+
+			foundCountries = countriesJson.countries;
+		} catch (error) {
+			console.error({ error });
+			foundCountries = [];
+		}
 	}
 
 	$: console.log(searchTerm, { foundCountries });
 </script>
 
-<div class="w-full relative bg-blue-400">
+<div class="w-full relative">
 	<label for="search-country" class="flex items-center gap-4 h-12 w-5/12 rounded shadow bg-white">
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
@@ -39,12 +50,14 @@
 			on:input={searchCountry}
 		/>
 	</label>
-	<ul class="bg-blue-100 w-5/12 absolute top-full rounded shadow-xl">
-		<li class="px-2 py-2">
-			<a href="/countries/ECU" class="flex items-center gap-4">
-				<img src="https://flagcdn.com/ec.svg" alt="Ecuador" class="w-12 h-10 object-cover" />
-				<span>Ecuador</span>
-			</a>
-		</li>
+	<ul class="bg-lighter-gray w-5/12 absolute top-full rounded shadow-xl">
+		{#each foundCountries as country}
+			<li class="px-2 py-2">
+				<a href={`/countries/${country.cca3}`} class="flex items-center gap-4">
+					<img src={country.flags.svg} alt={country.name.common} class="w-14 h-10 object-cover" />
+					<span>{country.name.common}</span>
+				</a>
+			</li>
+		{/each}
 	</ul>
 </div>
